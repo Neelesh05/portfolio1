916eb3e1d3e06097744ee17207e280d6f9c6402b
Neelesh05 <143531634+Neelesh05@users.noreply.github.com>
2025-11-13 12:31:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
